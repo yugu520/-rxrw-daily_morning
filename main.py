6c0c21b7822d3f8badba89c607b4bd3a94408bd5
FishBone <33279346+yugu520@users.noreply.github.com>
2023-05-10 01:28:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ def get_birthday():
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
+  print("words.status_code：" + words.status_code)
   if words.status_code != 200:
     return get_words()
   return words.json()['data']['text']
