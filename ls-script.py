d7df686cbc7ba84d6891c6f6a8dc153a57935154
FishBone <33279346+yugu520@users.noreply.github.com>
2024-07-18 23:05:42 +0800

    Update ls-script.py

--- ls-script.py
+++ ls-script.py
@@ -23,12 +23,61 @@ def get_weather():
   print('获取天气结果JSON：',res.json())
   weather = res.json()['forecasts'][0]
   return weather['city'], weather['casts']
+  
+def calcPrice():
+    # 链接
+    url = 'https://www.ly.com/flights/itinerary/roundtrip/HGH-SYX?date=2024-07-26,2024-07-29&from=%E8%90%A7%E5%B1%B1%E6%9C%BA%E5%9C%BA&to=%E5%87%A4%E5%87%B0%E6%9C%BA%E5%9C%BA&fromairport=HGH&toairport=SYX&p=&childticket=1,0'
+
+    # 发送GET请求
+    response = requests.get(url)
+
+    # 解析HTML内容
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # 查找所有具有特定类的标签
+    airs_tags = soup.find_all('p', class_='flight-item-name')
+    for tag in airs_tags:
+        if tag.text == '厦门航空MF8321':
+            air_name = tag.text
+            index = airs_tags.index(tag)
+            price_tags = soup.find_all('div', class_='head-prices')
+            start_times = soup.find_all('div', class_='f-startTime f-times-con')
+            end_times = soup.find_all('div', class_='f-endTime f-times-con')
+            start_time = start_times[index].text.split(" ")[0]
+            start_station = start_times[index].text.split(" ")[1]
+            end_time = end_times[index].text.strip().split(" ")[0]
+            end_station = end_times[index].text.strip().split(" ")[1]
+            price = price_tags[index].text.strip()
+            print(f"航空公司：{air_name}\n起飞时间：{start_time}\n起飞机场：{start_station}\n到达时间：{end_time}\n到达机场：{end_station}\n当天机票价格为：{price}")
+    return air_name, start_time, start_station, end_time, end_station, price
 
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 city, casts = get_weather()
-data = {"city":{"value":city},"date0":{"value":casts[0]['date']},"date1":{"value":casts[1]['date']},"date2":{"value":casts[2]['date']},"date3":{"value":casts[3]['date']},"dayweather0":{"value":casts[0]['dayweather']},"dayweather1":{"value":casts[1]['dayweather']},"dayweather2":{"value":casts[2]['dayweather']},"dayweather3":{"value":casts[3]['dayweather']},"daytemp0":{"value":casts[0]['daytemp_float']},"daytemp1":{"value":casts[1]['daytemp_float']},"daytemp2":{"value":casts[2]['daytemp_float']},"daytemp3":{"value":casts[3]['daytemp_float']}}
+air_name, start_time, start_station, end_time, end_station, price = calcPrice()
+
+data = {
+    "city": {"value": city},
+    "date0": {"value": casts[0]['date']},
+    "date1": {"value": casts[1]['date']},
+    "date2": {"value": casts[2]['date']},
+    "date3": {"value": casts[3]['date']},
+    "dayweather0": {"value": casts[0]['dayweather']},
+    "dayweather1": {"value": casts[1]['dayweather']},
+    "dayweather2": {"value": casts[2]['dayweather']},
+    "dayweather3": {"value": casts[3]['dayweather']},
+    "daytemp0": {"value": casts[0]['daytemp_float']},
+    "daytemp1": {"value": casts[1]['daytemp_float']},
+    "daytemp2": {"value": casts[2]['daytemp_float']},
+    "daytemp3": {"value": casts[3]['daytemp_float']},
+    "air_name": {"value": air_name},
+    "start_time": {"value": start_time},
+    "start_station": {"value": start_station},
+    "end_time": {"value": end_time},
+    "end_station": {"value": end_station},
+    "price": {"value": price}
+}
 user_id_list = user_ids.split(",")
 for user_id in user_id_list:
   res = wm.send_template(user_id, template_id, data)
